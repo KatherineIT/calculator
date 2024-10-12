b2241531daa2b2e0a28c4f7ed4d59a4ba142806f
akate <a.katenka.99@gmail.com>
2024-10-12 15:16:36 +0300

    edits made

--- src/main/java/by/arsenteva/calapp/service/ResultWriterService.java
+++ src/main/java/by/arsenteva/calapp/service/ResultWriterService.java
@@ -1,12 +1,12 @@
 package by.arsenteva.calapp.service;
 
 public class ResultWriterService {
-    public static void printNumber(int num1, int num2){
+    public static void printNumber(int num1, int num2) {
         System.out.println("Число первое: " + num1);
         System.out.println("Число второе: " + num2);
     }
 
-    public static void printResult(String operation, int num1, int num2, int result){
-         System.out.println(operation + num1 + " и " + num2 + " равно " + result);
+    public static void printResult(String operation, int num1, int num2, int result) {
+        System.out.println(operation + num1 + " и " + num2 + " равно " + result);
     }
 }
--- src/main/java/by/arsenteva/calapp/util/Calculator.java
+++ src/main/java/by/arsenteva/calapp/util/Calculator.java
@@ -1,24 +1,16 @@
 package by.arsenteva.calapp.util;
 
-import by.arsenteva.calapp.service.ResultWriterService;
-
 public class Calculator {
-    public static void main(String[] args) {
-
-    }
 
     public static int sum(int x, int y) {
-        int result1 = x + y;
-        return result1;
+       return x + y;
     }
 
     public static int subtraction(int x, int y) {
-        int result2 = x - y;
-        return result2;
+        return x - y;
     }
 
     public static int multiplication(int x, int y) {
-        int result3 = x * y;
-        return result3;
+        return x * y;
     }
 }
